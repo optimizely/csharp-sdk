52907721ee3efdc64f706d6cebc32a9eb46fccfc
FOLIO3PK\ialam <ialam@folio3.com>
2017-05-03 16:41:49 +0500

    nuget spec for multiple platform and PS script for creating package from spec file

---
+++ OptimizelySDK.Package/pack.ps1
@@ -0,0 +1,22 @@
+$msBuildLocation="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+
+Write-Host "Packing Optimizely SDK for NuGet"
+Write-Host "-"
+Write-Host "This script requires VS 2017 Community Edition & NuGet CLI"
+Write-Host "-"
+Write-Host "-"
+
+& $msBuildLocation ..\OptimizelySDK.sln /p:Platform="Any CPU" /p:Configuration=Release /p:GenerateDocumentation=true /nr:false /clp:Summary
+
+Write-Host "-"
+Write-Host "-"
+Write-Host "Build complete. Copying files..."
+
+Copy-Item  -Path "..\OptimizelySDK\bin\Release\OptimizelySDK.dll" -Destination ".\lib\net45"  -Recurse -force
+Copy-Item  -Path "..\OptimizelySDK.NetStandard16\bin\Release\netstandard1.6\OptimizelySDK.NetStandard16.dll" -Destination ".\lib\netstandard1.6"  -Recurse -force
+
+Write-Host "-"
+Write-Host "-"
+Write-Host "Creating NuGet package"
+
+.\nuget pack OptimizelySDK.nuspec
