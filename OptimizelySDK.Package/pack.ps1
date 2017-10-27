8922c4ccd3f41ec7aed1c563783f174f1ecbc230
Kelly Roach <kelly.roach@optimizely.com>
2017-10-27 14:30:38 -0700

    Add strong naming (sn.exe) calls to pack.ps1 .

--- OptimizelySDK.Package/pack.ps1
+++ OptimizelySDK.Package/pack.ps1
@@ -3,18 +3,51 @@ Write-Host "-"
 Write-Host "This script requires VS 2017 MSBuild & NuGet CLI"
 Write-Host "-"
 
+################################################################
+# Locate Tools (*.exe)
+################################################################
 if ($PSVersionTable["Platform"] -eq "Unix") {
     # Including macOS
-    $msBuildLocation="/Library/Frameworks/Mono.framework/Versions/Current/Commands/msbuild"
+    $msbuild="/Library/Frameworks/Mono.framework/Versions/Current/Commands/msbuild"
+} elseif (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise") {
+    $msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe"
+} elseif (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community") {
+    $msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
 } else {
-    $msBuildLocation="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+    Write-Host "Unable to locate msbuild.exe"
+    Exit 1
 }
 
-& $msBuildLocation ..\OptimizelySDK.sln /p:Platform="Any CPU" /p:Configuration=Release /p:GenerateDocumentation=true  /t:Clean,Build /nr:false /clp:Summary
+if ($PSVersionTable["Platform"] -eq "Unix") {
+    # Including macOS
+    $sn="/Library/Frameworks/Mono.framework/Versions/Current/Commands/sn"
+} elseif (Test-Path "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64") {
+    $sn="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe"
+} else {
+    Write-Host "Unable to locate sn.exe"
+    Exit 1
+}
 
+################################################################
+# Compiling (msbuild.exe)
+################################################################
+#& $msbuild ..\OptimizelySDK.sln /p:Platform="Any CPU" /p:Configuration=Release /p:GenerateDocumentation=true  /t:Clean,Build /nr:false /clp:Summary
 Write-Host "-"
 Write-Host "Build complete. Copying files..."
 
+################################################################
+# Strong Naming (sn.exe)
+################################################################
+# One can use 'find . -name "Optimizely*.dll" -print | grep "bin/Release" | grep -v ".Tests"'
+# to find the Optimizely*.dll that we need this PowerShell script to sign.
+& $sn  -R "../OptimizelySDK/bin/Release/OptimizelySDK.dll" "../keypair.snk"
+& $sn  -R "../OptimizelySDK.Net35/bin/Release/OptimizelySDK.Net35.dll" "../keypair.snk"
+& $sn  -R "../OptimizelySDK.Net40/bin/Release/OptimizelySDK.Net40.dll" "../keypair.snk"
+& $sn  -R "../OptimizelySDK.NetStandard16/bin/Release/netstandard1.6/OptimizelySDK.NetStandard16.dll" "../keypair.snk"
+
+################################################################
+# NuGet Packaging
+################################################################
 New-Item -Path ".\content" -ItemType "directory" -force
 Copy-Item -Path ".\Licenses" -Destination ".\content" -Recurse -force
 New-Item -Path ".\content\Licenses\Optimizely.SDK" -ItemType "directory" -force
