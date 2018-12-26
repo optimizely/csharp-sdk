d20c5854ead1d6c7723a2c25b454dcec1aa9de94
mfahadahmed <fahmed@folio3.com>
2018-12-26 13:04:33 +0500

    feat(event_builder): Don't track NaN, infinity and value greater than 2^53.

--- OptimizelySDK/Utils/Validator.cs
+++ OptimizelySDK/Utils/Validator.cs
@@ -111,8 +111,38 @@ namespace OptimizelySDK.Utils
         public static bool IsUserAttributeValid(KeyValuePair<string, object> attribute)
         {
             return (attribute.Key != null) && 
-                (attribute.Value is int || attribute.Value is string || attribute.Value is double 
-                 || attribute.Value is bool || attribute.Value is float || attribute.Value is long);
+                (attribute.Value is string || attribute.Value is bool || IsValidNumericValue(attribute.Value));
+        }
+
+        /// <summary>
+        /// Validates if the type of provided value is numeric.
+        /// </summary>
+        /// <param name="value">true if the type of provided value is numeric, false otherwise</param>
+        /// <returns></returns>
+        public static bool IsNumericType(object value)
+        {
+            return value is byte || value is sbyte || value is char || value is short || value is ushort
+                || value is int || value is uint || value is long || value is ulong || value is float
+                || value is double || value is decimal;
+        }
+
+        /// <summary>
+        /// Validates if the provided value is a valid numeric value.
+        /// </summary>
+        /// <param name="value">Input value</param>
+        /// <returns>true if the provided absolute value is not infinite, NAN and greater than 2^53, false otherwise</returns>
+        public static bool IsValidNumericValue(object value)
+        {
+            if (IsNumericType(value))
+            {
+                var doubleValue = Convert.ToDouble(value);
+                if (double.IsInfinity(doubleValue) || double.IsNaN(doubleValue) || Math.Abs(doubleValue) > Math.Pow(2, 53))
+                    return false;
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
