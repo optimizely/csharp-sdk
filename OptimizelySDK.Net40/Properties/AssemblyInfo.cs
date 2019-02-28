0e418fa7a0f8868a5ea980b6dae2e224acbb7ab8
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-02-28 13:48:15 -0800

    chore(update copyright)

--- OptimizelySDK.Net40/Properties/AssemblyInfo.cs
+++ OptimizelySDK.Net40/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OptimizelySDK.Net40")]
-[assembly: AssemblyCopyright("Copyright © 2017-2018")]
+[assembly: AssemblyCopyright("Copyright © 2017-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
