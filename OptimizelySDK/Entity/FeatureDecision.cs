90c340c798af291f92e803d874cffe8a3a42c32d
mfahadahmed <fahmed@folio3.com>
2019-03-25 17:11:29 +0500

    Update header.

--- OptimizelySDK/Entity/FeatureDecision.cs
+++ OptimizelySDK/Entity/FeatureDecision.cs
@@ -1,5 +1,5 @@
 ﻿/* 
- * Copyright 2017, Optimizely
+ * Copyright 2017, 2019, Optimizely
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-using System;
-using System.Collections.Generic;
 
 namespace OptimizelySDK.Entity
 {
