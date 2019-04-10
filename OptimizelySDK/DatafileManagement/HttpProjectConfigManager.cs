e385a95ec4043aa4b775bdf28357e49ca3024996
Sohail Hussain <mirza.sohailhussain@gmail.com>
2019-04-09 23:02:10 -0700

    added builder class

--- OptimizelySDK/DatafileManagement/HttpProjectConfigManager.cs
+++ OptimizelySDK/DatafileManagement/HttpProjectConfigManager.cs
@@ -1,11 +1,106 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 namespace OptimizelySDK.DatafileManagement
 {
     public class HttpProjectConfigManager : PollingProjectConfigManager
     {
+        public class Builder
+        {
+            private String SdkKey;
+            private String Url;
+            private String Format = "https://cdn.optimizely.com/datafiles/%s.json";
+            private bool AutoUpdate = true;
+            private TimeSpan Period = TimeSpan.FromSeconds(5);
+
+            public Builder WithSdkKey(String sdkKey)
+            {
+                SdkKey = sdkKey;
+                return this;
+            }
+
+            public Builder withUrl(String url)
+            {
+                Url = url;
+                return this;
+            }
+
+            public Builder withFormat(String format)
+            {
+                Format = format;
+                return this;
+            }
+
+            public Builder WithPollingInterval(TimeSpan period)
+            {
+                Period = period;
+
+                return this;
+            }
+            public Builder WithAutUpdate(bool autoUpdate)
+            {
+                AutoUpdate = autoUpdate;
+
+                return this;
+            }
+
+            public HttpProjectConfigManager build()
+            {
+                if (Url != null) {
+                    return new HttpProjectConfigManager(Url, Period, AutoUpdate);
+                }
+
+                if (SdkKey == null) {
+                    throw new Exception("sdkKey cannot be null", null);
+                }
+
+                Url = String.Format(Format, SdkKey);
+
+                return new HttpProjectConfigManager(Url, Period, AutoUpdate);
+            }
+        }
+
+        public HttpClient Client;
+        private string Url;
+        private string lastModifiedSince = String.Empty;
+
+        public HttpProjectConfigManager(string url, TimeSpan period,  bool autoUpdate) : base(period, autoUpdate)
+        {
+            Client = new HttpClient();
+            Url = url;
+        }
+
+        public Task OnReady {
+            get {
+                return _onReady;
+            }
+        }
+
         protected override ProjectConfig FetchConfig()
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage {
+                RequestUri = new Uri(Url),
+                Method = HttpMethod.Get
+            };
+
+            // Need to check empty works or not.
+            request.Headers.Add("last-modified", lastModifiedSince);
+
+            var httpResponse = Client.SendAsync(request);
+            httpResponse.Wait();
+
+            var response = httpResponse.Result;
+
+            if (response.StatusCode == HttpStatusCode.NotModified)
+            {
+                return null;
+            }
+            // TODO: Initialize DataFileProjectConfig
+
+            return null;
         }
     }
 }
--- OptimizelySDK/DatafileManagement/PollingProjectConfigManager.cs
+++ OptimizelySDK/DatafileManagement/PollingProjectConfigManager.cs
@@ -1,20 +1,98 @@
 ﻿using System;
 using System.Timers;
+using System.Threading.Tasks;
 
 namespace OptimizelySDK.DatafileManagement
 {
     public abstract class PollingProjectConfigManager : Timer, ProjectConfigManager
     {
+        protected Task _onReady;
+        private bool isStarted = false;
+        private bool autoUpdate = false;
+        private ProjectConfig currentProjectConfig = null;
+
+        public delegate void EventHandler();
+        public event EventHandler UpdateHandler;
+
+        public PollingProjectConfigManager(TimeSpan period, bool autoUpdate)
+        {
+            //this.AutoReset = autoUpdate;
+            // When app finishes task then start polling,
+            // not at fixed interval
+            this.autoUpdate = autoUpdate;
+            this.Interval = period.TotalMilliseconds;
+            this.AutoReset = false;
+            this.Elapsed += Run;
+        }
+
+        public new void Stop() 
+        {
+            isStarted = false;
+            base.Stop();
+        }
+
         public ProjectConfig GetConfig()
         {
-            return null;
+            if (isStarted) 
+            {
+                try {
+                    _onReady.Wait();
+                } catch (Exception ex) {
+                    Console.WriteLine(ex);
+                    //logger.warn("Interrupted waiting for valid ProjectConfig");
+                }
+
+                return currentProjectConfig;
+            }
+
+            // Block execution
+            ProjectConfig projectConfig = FetchConfig();
+
+            return projectConfig == null ? currentProjectConfig : projectConfig;
         }
 
         public bool SetConfig(ProjectConfig projectConfig)
         {
-            return false;
+            if (projectConfig == null) {
+                return false;
+            }
+
+            //String previousVersion = currentProjectConfig.get() == null ? "null" : currentProjectConfig.get().getRevision();
+
+            //if (projectConfig.getRevision().equals(previousVersion)) {
+            //    return false;
+            //}
+
+            //logger.info("New datafile set with revision: {}. Old version: {}", projectConfig.getRevision(), previousVersion);
+
+            currentProjectConfig = projectConfig;
+
+            return true;
         }
 
         protected abstract ProjectConfig FetchConfig();
+
+        private void Run(object sender, ElapsedEventArgs e)
+        {
+            // 4.0 < frameworks
+            Task t = new Task(() => {
+                // if not modified, no need to send
+                var fetchConfig = this.FetchConfig();
+                if (fetchConfig == null) return;
+                this.SetConfig(fetchConfig);
+            });
+
+            t.ContinueWith((arg) => {
+                if(autoUpdate) Start();
+            });
+
+            if (isStarted == false)
+            {
+                isStarted = true;
+                _onReady = t;
+            }
+
+            t.Start();
+        }
     }
 }
