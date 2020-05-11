22045d65a95646e8aa1769c5227a2fcb4a92fff2
mnoman09 <Muhammadnoman@folio3.com>
2020-05-11 11:50:15 +0500

    Feat: Added OptimizelyJson class to support GetFeatureVariableJson (#214)

--- OptimizelySDK/Exceptions/OptimizelyException.cs
+++ OptimizelySDK/Exceptions/OptimizelyException.cs
@@ -1,5 +1,5 @@
 ﻿/* 
- * Copyright 2017, Optimizely
+ * Copyright 2017, 2020, Optimizely
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,6 +29,14 @@ namespace OptimizelySDK.Exceptions
     public class OptimizelyRuntimeException : OptimizelyException
     {
         public OptimizelyRuntimeException(string message)
+            : base(message)
+        {
+        }
+    }
+    
+    public class InvalidJsonException : OptimizelyException
+    {
+        public InvalidJsonException(string message)
             : base(message)
         {
         }
