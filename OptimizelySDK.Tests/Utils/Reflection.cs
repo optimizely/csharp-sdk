62ba2fa9c167273722846e27b1462b1e59437b64
muhammadnoman <muhammadnoman@folio3.com>
2020-06-26 13:37:41 +0500

    added comments

--- OptimizelySDK.Tests/Utils/Reflection.cs
+++ OptimizelySDK.Tests/Utils/Reflection.cs
@@ -8,10 +8,10 @@ namespace OptimizelySDK.Tests.Utils
     public static class Reflection
     {
         /// <summary>
-        /// Returns non-public field value.
+        /// Returns field value.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="type">Object metadata information</param>
+        /// <param name="obj">Object from which you want to get variable</param>
         /// <param name="fieldName">Name of the field</param>
         /// <returns></returns>
         public static T GetFieldValue<T, U>(U obj, string fieldName)
@@ -27,12 +27,19 @@ namespace OptimizelySDK.Tests.Utils
             return (T)default(T);
         }
 
-        public static T GetPropertyValue<T, U>(U obj, string fieldName)
+        /// <summary>
+        /// Returns property value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">Object from which you want to get property value.</param>
+        /// <param name="propertyName">Name of the property</param>
+        /// <returns></returns>
+        public static T GetPropertyValue<T, U>(U obj, string propertyName)
         {
             var propertyInfo = GetAllProperties(typeof(U));
             foreach (var popertyInfo in propertyInfo)
             {
-                if (popertyInfo.Name == fieldName)
+                if (popertyInfo.Name == propertyName)
                 {
                     return (T)popertyInfo.GetValue(obj);
                 }
@@ -40,21 +47,31 @@ namespace OptimizelySDK.Tests.Utils
             return (T)default(T);
         }
 
-        public static IEnumerable<PropertyInfo> GetAllProperties(Type t)
+        /// <summary>
+        /// Returns all properties info of provided type.
+        /// </summary>
+        /// <param name="type"> Type of which to Get all properties info.</param>
+        /// <returns>IEnumerable<PropertyInfo></returns>
+        public static IEnumerable<PropertyInfo> GetAllProperties(Type type)
         {
-            if (t == null)
+            if (type == null)
                 return Enumerable.Empty<PropertyInfo>();
-            BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic |
+            var flags = BindingFlags.Public | BindingFlags.NonPublic |
                                  BindingFlags.Static | BindingFlags.Instance |
                                  BindingFlags.DeclaredOnly;
-            return t.GetProperties(flags).Concat(GetAllProperties(t.BaseType));
+            return type.GetProperties(flags).Concat(GetAllProperties(type.BaseType));
         }
 
+        /// <summary>
+        /// Returns all fields info of provided type.
+        /// </summary>
+        /// <param name="type"> Type of which to Get all fields info.</param>
+        /// <returns>IEnumerable<FieldInfo></returns>
         public static IEnumerable<FieldInfo> GetAllFields(Type t)
         {
             if (t == null)
                 return Enumerable.Empty<FieldInfo>();
-            BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic |
+            var flags = BindingFlags.Public | BindingFlags.NonPublic |
                                  BindingFlags.Static | BindingFlags.Instance |
                                  BindingFlags.DeclaredOnly;
             return t.GetFields(flags).Concat(GetAllFields(t.BaseType));
