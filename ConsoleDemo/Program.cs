5dee709d3e9b3b024232a7e08aeb9e12c9bd2967
Dustin Sier <Dustin.Sier@episerver.com>
2021-08-17 15:07:49 -0400

    Console demo

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿//Import Optimizely SDK
+using OptimizelySDK;
+using OptimizelySDK.Entity;
+using System;
+
+namespace ConsoleDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Instantiate an Optimizely client
+            var optimizelyInstance = OptimizelyFactory.NewDefaultInstance("<YOUR SDK KEY>");
+            var attributes = new UserAttributes();
+            attributes.Add("logged_in", true);
+
+            // Create a user context
+            var user = optimizelyInstance.CreateUserContext("user123", attributes);
+
+            var decision = user.Decide("product_sort");
+
+            Console.WriteLine("Enabled: " + decision.Enabled);
+            Console.WriteLine("Flag Key: " + decision.FlagKey);
+        }
+    }
+}
