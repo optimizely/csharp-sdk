c1233ce7f32dd7222dde875a95fb65d13fbf5cd2
Daniel Pons <86228191+daniel-pons@users.noreply.github.com>
2021-09-13 18:19:58 +0200

    Create virtual methods on user context (#280)
    * feat(common): Changed public members on OptimizelyUserContext to be virtual
    Make OptimizelyUserContext members virtual, making them overridable by testing frameworks
    
    * fix(tests): Run tests on English culture Set the current thread culture to english, as for systems on others cultures, some tests were failing.

---
+++ OptimizelySDK.Tests/TestSetup.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Globalization;
+using System.Threading;
+
+namespace OptimizelySDK.Tests
+{
+    [SetUpFixture]
+    public class TestSetup
+    {
+        [SetUp]
+        public void Init()
+        {
+            /* There are some issues doing assertions on tests with floating point numbers using the .ToString()
+             * method, as it's culture dependent. EG: TestGetFeatureVariableValueForTypeGivenFeatureFlagIsNotEnabledForUser,
+             * assigning the culture to English will make this kind of tests to work on others culture based systems. */
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            // Empty, but required: https://nunit.org/nunitv2/docs/2.6.4/setupFixture.html
+        }
+    }
+}
