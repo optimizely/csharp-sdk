86f8ea57692512338b20154bb9e03792eed68c47
dustin-sier <dustin.sier@optimizely.com>
2021-11-03 14:06:45 -0400

    fix rule key being non-null

--- OptimizelySDK/Optimizely.cs
+++ OptimizelySDK/Optimizely.cs
@@ -830,7 +830,7 @@ namespace OptimizelySDK
             var variationKey = decision?.Variation?.Key;
 
             // TODO: add ruleKey values when available later. use a copy of experimentKey until then.
-            var ruleKey = decision?.Experiment?.Key;
+            var ruleKey = decision?.Experiment != null ? decision?.Experiment?.Key : null;
 
             var decisionInfo = new Dictionary<string, object>
             {
