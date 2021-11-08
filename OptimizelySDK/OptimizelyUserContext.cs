d78a22bc3a27b8f9258ef4991bbcd1c0c8502857
dustin-sier <dustin.sier@optimizely.com>
2021-11-08 11:12:58 -0500

    Fixed content of message

--- OptimizelySDK/OptimizelyUserContext.cs
+++ OptimizelySDK/OptimizelyUserContext.cs
@@ -327,7 +327,7 @@ namespace OptimizelySDK
                 if (variation != null)
                 {
                     reasons.AddInfo("Decided by forced decision.");
-                    reasons.AddInfo("Variation ({0}) is mapped to ({1}) and user ({2}) in the forced decision map", variationKey, context.FlagKey, this.UserId);
+                    reasons.AddInfo("Variation ({0}) is mapped to flag ({1}) and user ({2}) in the forced decision map", variationKey, context.FlagKey, this.UserId);
                     return Result<Variation>.NewResult(variation, reasons);
                 }
                 else
