375d7489172c2ea97989007ae1fecb4086f9d374
dustin-sier <dustin.sier@optimizely.com>
2021-11-29 09:02:51 -0500

    changed to experiment?.id

--- OptimizelySDK/Event/Builder/EventBuilder.cs
+++ OptimizelySDK/Event/Builder/EventBuilder.cs
@@ -132,7 +132,7 @@ namespace OptimizelySDK.Event.Builder
                     new Dictionary<string, object>
                     {
                         { Params.CAMPAIGN_ID,   experiment?.LayerId },
-                        { Params.EXPERIMENT_ID, experiment != null ? experiment.Id : "" },
+                        { Params.EXPERIMENT_ID, experiment?.Id},
                         { Params.VARIATION_ID,  variationId }
                     }
             };
