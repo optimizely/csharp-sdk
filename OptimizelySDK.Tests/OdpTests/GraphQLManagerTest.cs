1e579ee3d63d6678665ca90503745b1bb7713cee
Mike Chu <michael.chu@optimizely.com>
2022-08-03 14:29:21 -0400

    Fully cover json response

--- OptimizelySDK.Tests/OdpTests/GraphQLManagerTest.cs
+++ OptimizelySDK.Tests/OdpTests/GraphQLManagerTest.cs
@@ -1,7 +1,9 @@
 ﻿using Moq;
 using NUnit.Framework;
+using OptimizelySDK.AudienceConditions;
 using OptimizelySDK.Logger;
 using OptimizelySDK.Odp;
+using System.Security;
 
 namespace OptimizelySDK.Tests.OdpTests
 {
@@ -9,7 +11,7 @@ namespace OptimizelySDK.Tests.OdpTests
     public class GraphQLManagerTest
     {
         private Mock<ILogger> MockLogger;
-        
+
         [SetUp]
         public void Setup()
         {
@@ -21,7 +23,8 @@ namespace OptimizelySDK.Tests.OdpTests
         public void ShouldParseSuccessfulResponse()
         {
             #region const string responseJson
-            const string responseJson = @"
+
+            var responseJson = @"
 {
     ""data"": {
         ""customer"": {
@@ -44,19 +47,32 @@ namespace OptimizelySDK.Tests.OdpTests
         }
     }
 }";
+
             #endregion
+
             var manager = new GraphQLManager(MockLogger.Object);
 
             var response = manager.ParseResponse(responseJson);
-            
-            Assert.IsNull(response.Errors); 
+
+            Assert.IsNull(response.Errors);
             Assert.IsNotNull(response.Data);
+            Assert.IsNotNull(response.Data.Customer);
+            Assert.IsNotNull(response.Data.Customer.Audiences);
+            Assert.IsNotNull(response.Data.Customer.Audiences.Edges);
+            Assert.IsTrue(response.Data.Customer.Audiences.Edges.Length == 2);
+            var node = response.Data.Customer.Audiences.Edges[0].Node;
+            Assert.IsTrue(node.Name == "has_email");
+            Assert.IsTrue(node.State == "qualified");
+            node = response.Data.Customer.Audiences.Edges[1].Node;
+            Assert.IsTrue(node.Name == "has_email_opted_in");
+            Assert.IsTrue(node.State == "qualified");
         }
-        
+
         [Test]
         public void ShouldParseErrorResponse()
         {
             #region const string responseJson
+
             const string responseJson = @"
 {
    ""errors"": [
@@ -69,8 +85,8 @@ namespace OptimizelySDK.Tests.OdpTests
                 }
             ],
             ""path"": [
-            ""customer""
-                ],
+                ""customer""
+            ],
             ""extensions"": {
                 ""classification"": ""InvalidIdentifierException""
             }
@@ -80,13 +96,25 @@ namespace OptimizelySDK.Tests.OdpTests
         ""customer"": null
     }
 }";
+
             #endregion
+
             var manager = new GraphQLManager(MockLogger.Object);
 
             var response = manager.ParseResponse(responseJson);
-            
+
             Assert.IsNull(response.Data.Customer);
             Assert.IsNotNull(response.Errors);
+            Assert.IsTrue(response.Errors.Length == 1);
+            var error = response.Errors[0];
+            Assert.IsTrue(error.Message.Contains("asdsdaddddd"));
+            Assert.IsTrue(error.Locations.Length == 1);
+            var location = error.Locations[0];
+            Assert.IsTrue(location.Line == 2);
+            Assert.IsTrue(location.Column == 3);
+            Assert.IsTrue(error.Path.Length == 1);
+            Assert.IsTrue(error.Path[0] == "customer");
+            Assert.IsTrue(error.Extensions.Classification=="InvalidIdentifierException");
         }
     }
 }
