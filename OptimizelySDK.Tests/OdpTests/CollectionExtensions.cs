b987d9d1f60efeb79a64479ab9010ac7babb6a05
Mike Chu <michael.chu@optimizely.com>
2022-08-05 17:32:02 -0400

    Add helper extensions

---
+++ OptimizelySDK.Tests/OdpTests/CollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+namespace OptimizelySDK.Tests.OdpTests
+{
+    public static class CollectionTestExtensions
+    {
+        public static string[] ToKeyCollection(this ICollection keys)
+        {
+            var keyArray = new string[keys.Count];
+            keys.CopyTo(keyArray, 0);
+            return keyArray;
+        }
+    }
+}
---
+++ OptimizelySDK/Odp/DateTimeExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OptimizelySDK.Odp
+{
+    public static class DateTimeExtension
+    {
+        // Because we're only on .NET Framework 4.5
+        public static long ToUnixTimeMilliseconds(this DateTime dateTime)
+        {
+            var unixTimeStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return Convert.ToInt64(dateTime.Subtract(unixTimeStart).TotalMilliseconds);
+        }
+    }
+}
