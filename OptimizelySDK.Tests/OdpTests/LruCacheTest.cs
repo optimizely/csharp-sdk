671e159869912ad8f37edf15a65040927225d98e
Mike Chu <michael.chu@optimizely.com>
2022-08-05 13:28:11 -0400

    Skeleton

---
+++ OptimizelySDK.Tests/OdpTests/LruCacheTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace OptimizelySDK.Tests.OdpTests
+{
+    [TestFixture]
+    public class LruCacheTest
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void ShouldCreateSaveAndLookupOneItem() { }
+
+        [Test]
+        public void ShouldSaveAndLookupMultipleItems() { }
+        
+        [Test]
+        public void ShouldHandleWhenCacheIsDisabled() { }
+        
+        [Test]
+        public void ShouldThrowWhenItemsExpire() { }
+        
+        [Test]
+        public void ShouldHandleWhenCacheReachesMaxSize() { }
+        
+        [Test]
+        public void ShouldHandleWhenMaxSizeIsReducedInBetween() { }
+
+        [Test]
+        public void ShouldHandleWhenCacheIsReset() { }
+    }
+}
