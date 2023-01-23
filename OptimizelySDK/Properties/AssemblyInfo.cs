b540978508829e49b48c69de4c5a8cb4f316b933
Mike Chu <michael.chu@optimizely.com>
2023-01-23 16:57:39 -0500

    Testing InternalsVisibleTo in csproj

--- OptimizelySDK/Properties/AssemblyInfo.cs
+++ OptimizelySDK/Properties/AssemblyInfo.cs
@@ -23,7 +23,6 @@ using System.Runtime.InteropServices;
 // OptimizelySDK.Tests unit tests. 
 #pragma warning disable 1700
 [assembly: InternalsVisibleTo("OptimizelySDK.Tests")]
-[assembly: InternalsVisibleTo("OptimizelySDK.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100355b847429963d253d4ab3aa0fdcf8ca7010815857a9de53c262eef801de76e7be4e294e487daee54e535d530ace0d4918b75cfc470142630058ba34a327eefc896ec232301c4e1d16fcf35e292c3a0bdec6b43a0e2d85f679efd4ee77465a23cf866bebafd814e5d555ffbac66f228a75ef60aefffe51c6091103d1455febf5")]
 #pragma warning restore 1700
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
