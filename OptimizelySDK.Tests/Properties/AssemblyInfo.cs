dd037353afd499449a79c195e4be586f70e5b014
Mike Chu <104384559+mikechu-optimizely@users.noreply.github.com>
2023-03-24 10:17:23 -0400

    [FSSDK-9006] Increase build/revision (#341)
    to republish to NuGet

--- OptimizelySDK.Tests/Properties/AssemblyInfo.cs
+++ OptimizelySDK.Tests/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.11.2.0")]
-[assembly: AssemblyFileVersion("3.11.2.0")]
-[assembly: AssemblyInformationalVersion("3.11.2")] // Used by Nuget.
+[assembly: AssemblyVersion("3.11.2.1")]
+[assembly: AssemblyFileVersion("3.11.2.1")]
+[assembly: AssemblyInformationalVersion("3.11.2.1")] // Used by Nuget.
