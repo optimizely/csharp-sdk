d8c871a2445ab70feb74c5eb3acd96807faf5bbd
Andy Leap <andrew.leap@optimizely.com>
2023-04-13 12:49:35 -0400

    add config check in odp methods

--- OptimizelySDK/Optimizely.cs
+++ OptimizelySDK/Optimizely.cs
@@ -1339,6 +1339,14 @@ namespace OptimizelySDK
             List<OdpSegmentOption> segmentOptions
         )
         {
+            var config = ProjectConfigManager?.GetConfig();
+
+            if (config == null)
+            {
+                Logger.Log(LogLevel.ERROR, "Datafile has invalid format. Failing 'FetchQualifiedSegments'.");
+                return null;
+            }
+
             return OdpManager?.FetchQualifiedSegments(userId, segmentOptions);
         }
 
@@ -1348,6 +1356,14 @@ namespace OptimizelySDK
         /// <param name="userId">FS User ID to send</param>
         internal void IdentifyUser(string userId)
         {
+            var config = ProjectConfigManager?.GetConfig();
+
+            if (config == null)
+            {
+                Logger.Log(LogLevel.ERROR, "Datafile has invalid format. Failing 'Identify'.");
+                return;
+            }
+
             OdpManager?.IdentifyUser(userId);
         }
 
@@ -1362,6 +1378,14 @@ namespace OptimizelySDK
             Dictionary<string, object> data
         )
         {
+            var config = ProjectConfigManager?.GetConfig();
+
+            if (config == null)
+            {
+                Logger.Log(LogLevel.ERROR, "Datafile has invalid format. Failing 'SendOdpEvent'.");
+                return;
+            }
+
             OdpManager?.SendEvent(type, action, identifiers, data);
         }
 #endif
