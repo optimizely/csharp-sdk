714492abad2063a98190e7e55e5bf31c210e5a3d
Mike Chu <michael.chu@optimizely.com>
2023-04-28 15:31:54 -0400

    Bump assembly versions

--- OptimizelySDK.DemoApp/Properties/AssemblyInfo.cs
+++ OptimizelySDK.DemoApp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OptimizelySDK.DemoApp")]
-[assembly: AssemblyCopyright("Copyright © 2017-2020")]
+[assembly: AssemblyCopyright("Copyright © 2017-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.11.2.0")]
-[assembly: AssemblyFileVersion("3.11.2.0")]
-[assembly: AssemblyInformationalVersion("3.11.2")] // Used by Nuget.
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyInformationalVersion("4.0.0-beta")] // Used by Nuget.
