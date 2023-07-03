5f49a7c80c3b7284dcdf4a509551d57115bf13aa
Mike Chu <michael.chu@optimizely.com>
2023-07-03 14:05:41 -0400

    Update assembly semantic version

--- OptimizelySDK.DemoApp/Properties/AssemblyInfo.cs
+++ OptimizelySDK.DemoApp/Properties/AssemblyInfo.cs
@@ -35,9 +35,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("3.11.2.1")]
-[assembly: AssemblyFileVersion("3.11.2.1")]
-[assembly: AssemblyInformationalVersion("3.11.2.1")] // Used by Nuget.
+[assembly: AssemblyVersion("3.11.3.0")]
+[assembly: AssemblyFileVersion("3.11.3.0")]
+[assembly: AssemblyInformationalVersion("3.11.3")] // Used by Nuget.
 
