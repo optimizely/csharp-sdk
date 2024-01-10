709b19164465d4170f7b44d32f38314398f13d7d
Mike Chu <michael.chu@optimizely.com>
2024-01-10 12:41:44 -0500

    test: More test cases

--- QuickStart/Program.cs
+++ QuickStart/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Newtonsoft.Json;
 using OptimizelySDK;
+using OptimizelySDK.Entity;
+using OptimizelySDK.Event;
+using OptimizelySDK.Notifications;
 
 namespace QuickStart
 {
@@ -12,10 +17,9 @@ namespace QuickStart
             var optimizelyClient = OptimizelyFactory.NewDefaultInstance("TbrfRLeKvLyWGusqANoeR");
             if (!optimizelyClient.IsValid)
             {
-                Console.WriteLine(
-                    "Optimizely client invalid. " +
-                    "Verify in Settings>Environments that you used the primary environment's SDK key");
+                Console.WriteLine("Optimizely client invalid. Verify in Settings>Environments that you used the primary environment's SDK key");
                 optimizelyClient.Dispose();
+                return;
             }
             
             const string USER_ID = "matjaz-user-2";
@@ -39,9 +43,9 @@ namespace QuickStart
             // Console.WriteLine(JsonConvert.SerializeObject(variables));
             
             // DecideForKeys
-            var keys = new[] {"flag1", "flag2"};
-            var decisions = user.DecideForKeys(keys);
-            Console.WriteLine(JsonConvert.SerializeObject(decisions));
+            // var keys = new[] {"flag1", "flag2"};
+            // var decisions = user.DecideForKeys(keys);
+            // Console.WriteLine(JsonConvert.SerializeObject(decisions));
             
             // DecideAll
             // var decisions = user.DecideAll();
@@ -60,7 +64,85 @@ namespace QuickStart
             // var variation = optimizelyClient.Activate("flag1", USER_ID);
             // Console.WriteLine("Variation: " + JsonConvert.SerializeObject(variation));
             
+            // NotificationCenter
+            // var callbacks = new TestNotificationCallbacks();
+            // // for Activate
+            // optimizelyClient.NotificationCenter.AddNotification(
+            //     NotificationCenter.NotificationType.Activate,
+            //     TestNotificationCallbacks.ActivateCallback);
+            // optimizelyClient.Activate("flag1", USER_ID);
+            // // for Track
+            // optimizelyClient.NotificationCenter.AddNotification(
+            //     NotificationCenter.NotificationType.Track,
+            //     TestNotificationCallbacks.TrackCallback);
+            // optimizelyClient.Track("myevent", USER_ID);
+            // // for Decision
+            // optimizelyClient.NotificationCenter.AddNotification(
+            //     NotificationCenter.NotificationType.Decision,
+            //     TestNotificationCallbacks.DecisionCallback);
+            // user.Decide("flag1");
+            // // for ConfigUpdate
+            // optimizelyClient.NotificationCenter.AddNotification(
+            //     NotificationCenter.NotificationType.OptimizelyConfigUpdate,
+            //     callbacks.ConfigUpdateCallback);
+            // // for LogEvent
+            // optimizelyClient.NotificationCenter.AddNotification(
+            //     NotificationCenter.NotificationType.LogEvent,
+            //     callbacks.LogEventCallback);
+            
             optimizelyClient.Dispose();
+        }
+    }
+    
+    public class TestNotificationCallbacks
+    {
+        public static void ActivateCallback(Experiment experiment, string userId,
+            UserAttributes userAttributes,
+            Variation variation, LogEvent logEvent
+        )
+        {
+            Console.WriteLine(">>> Activate Callback");
+            Console.WriteLine(experiment.Key);
+            Console.WriteLine(userId);
+            Console.WriteLine(userAttributes);
+            Console.WriteLine(variation.Key);
+            Console.WriteLine(JsonConvert.SerializeObject(logEvent));
+        }
+
+        public static void TrackCallback(string eventKey, string userId,
+            UserAttributes userAttributes,
+            EventTags eventTags, LogEvent logEvent
+        )
+        {
+            Console.WriteLine(">>> Track Callback");
+            Console.WriteLine(eventKey);
+            Console.WriteLine(userId);
+            Console.WriteLine(userAttributes);
+            Console.WriteLine(JsonConvert.SerializeObject(eventTags));
+            Console.WriteLine(JsonConvert.SerializeObject(logEvent));
+        }
+
+        public static void DecisionCallback(string type, string userId,
+            UserAttributes userAttributes,
+            Dictionary<string, object> decisionInfo
+        )
+        {
+            Console.WriteLine(">>> Decision Callback");
+            Console.WriteLine(type);
+            Console.WriteLine(userId);
+            Console.WriteLine(userAttributes);
+            Console.WriteLine(JsonConvert.SerializeObject(decisionInfo));
+        }
+
+        public void ConfigUpdateCallback()
+        {
+            Console.WriteLine(">>> Config Update Callback");
+        }
+
+        public void LogEventCallback(LogEvent logEvent)
+        {
+            Console.WriteLine(">>> Log Event Callback");
+            Console.WriteLine(JsonConvert.SerializeObject(logEvent));
         }
     }
 }
