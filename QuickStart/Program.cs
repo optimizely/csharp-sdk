59d0e1b43d41a66cf7916bcf8a4cd17a698c2398
Mike Chu <michael.chu@optimizely.com>
2024-01-10 11:58:39 -0500

    test: WIP validation before release

--- QuickStart/Program.cs
+++ QuickStart/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
 using OptimizelySDK;
 
 namespace QuickStart
@@ -7,8 +9,7 @@ namespace QuickStart
     {
         public static void Main()
         {
-            // This Optimizely initialization is synchronous. For other methods, see the C# SDK reference.
-            var optimizelyClient = OptimizelyFactory.NewDefaultInstance("K4UmaV5Pk7cEh2hbcjgwe");
+            var optimizelyClient = OptimizelyFactory.NewDefaultInstance("TbrfRLeKvLyWGusqANoeR");
             if (!optimizelyClient.IsValid)
             {
                 Console.WriteLine(
@@ -16,69 +17,49 @@ namespace QuickStart
                     "Verify in Settings>Environments that you used the primary environment's SDK key");
                 optimizelyClient.Dispose();
             }
+            
+            const string USER_ID = "matjaz-user-2";
 
-            var hasOnFlags = false;
+            var user = optimizelyClient.CreateUserContext(USER_ID);
 
-            /*
-             * To get rapid demo results, generate random users.
-             * Each user always sees the same variation unless you reconfigure the flag rule.
-             */
-            var rnd = new Random();
-            for (var i = 0; i < 10; i++)
-            {
-                var userId = rnd.Next(1000, 9999).ToString();
-
-                // Create a user context to bucket the user into a variation.
-                var user = optimizelyClient.CreateUserContext(userId);
-
-                // "product_sort" corresponds to a flag key in your Optimizely project
-                var decision = user.Decide("product_sort");
-
-                // Did decision fail with a critical error?
-                if (string.IsNullOrEmpty(decision.VariationKey))
-                {
-                    Console.WriteLine(Environment.NewLine + Environment.NewLine +
-                                      "Decision error: " + string.Join(" ", decision.Reasons));
-                    continue;
-                }
-
-                /*
-                 * Get a dynamic configuration variable.
-                 * "sort_method" corresponds to a variable key in your Optimizely project.
-                 */
-                var sortMethod = decision.Variables.ToDictionary()["sort_method"];
-
-                hasOnFlags = hasOnFlags || decision.Enabled;
-
-                /*
-                 * Mock what the user sees with print statements (in production, use flag variables to implement feature configuration)
-                 */
-
-                // always returns false until you enable a flag rule in your Optimizely project
-                Console.WriteLine(Environment.NewLine +
-                                  $"Flag {(decision.Enabled ? "on" : "off")}. " +
-                                  $"User number {user.GetUserId()} saw " +
-                                  $"flag variation {decision.VariationKey} and got " +
-                                  $"products sorted by {sortMethod} config variable as part of " +
-                                  $"flag rule {decision.RuleKey}");
-            }
-
-            if (!hasOnFlags)
-            {
-                var projectId = optimizelyClient.ProjectConfigManager.GetConfig().ProjectId;
-                var projectSettingsUrl =
-                    $"https://app.optimizely.com/v2/projects/{projectId}/settings/implementation";
-
-                Console.WriteLine(Environment.NewLine + Environment.NewLine +
-                                  "Flag was off for everyone. Some reasons could include:" +
-                                  Environment.NewLine +
-                                  "1. Your sample size of visitors was too small. Re-run, or increase the iterations in the FOR loop" +
-                                  Environment.NewLine +
-                                  "2. By default you have 2 keys for 2 project environments (dev/prod). Verify in Settings>Environments that you used the right key for the environment where your flag is toggled to ON." +
-                                  Environment.NewLine + Environment.NewLine +
-                                  $"Check your key at {projectSettingsUrl}");
-            }
-
+            // Fetch
+            // Console.WriteLine("Fetch:" + user.FetchQualifiedSegments());
+            // var qualifiedSegments = user.GetQualifiedSegments();
+            // Console.WriteLine(JsonConvert.SerializeObject(qualifiedSegments));
+            // const string SEGMENT_ID = "atsbugbashsegmentdob";
+            // Console.WriteLine($"Is Qualified for {SEGMENT_ID}: {user.IsQualifiedFor(SEGMENT_ID)}");
+            
+            // TrackEvent
+            // user.TrackEvent("myevent");
+            
+            // Decide
+            // var decision = user.Decide("flag1");
+            // Console.WriteLine(JsonConvert.SerializeObject(decision));
+            // var variables = decision.Variables.ToDictionary();
+            // Console.WriteLine(JsonConvert.SerializeObject(variables));
+            
+            // DecideForKeys
+            var keys = new[] {"flag1", "flag2"};
+            var decisions = user.DecideForKeys(keys);
+            Console.WriteLine(JsonConvert.SerializeObject(decisions));
+            
+            // DecideAll
+            // var decisions = user.DecideAll();
+            // Console.WriteLine(JsonConvert.SerializeObject(decisions));
+            
+            // Set & Get & Remove ForcedDecision
+            // var context = new OptimizelyDecisionContext("flag1", "default-rollout-34902-22583870382");
+            // var forcedDecision = new OptimizelyForcedDecision("off");
+            // user.SetForcedDecision(context, forcedDecision);
+            // var result = user.GetForcedDecision(context);
+            // Console.WriteLine(JsonConvert.SerializeObject(result));
+            // var wasRemoved = user.RemoveForcedDecision(context);
+            // Console.WriteLine($"Was removed: {wasRemoved}");
+            
+            // Activate
+            // var variation = optimizelyClient.Activate("flag1", USER_ID);
+            // Console.WriteLine("Variation: " + JsonConvert.SerializeObject(variation));
+            
             optimizelyClient.Dispose();
         }
     }
