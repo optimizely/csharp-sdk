ed24ffef3220fb834b771a8f99ea4581a5993493
Mike Chu <michael.chu@optimizely.com>
2024-11-06 16:33:34 -0500

    chore: bump semver 1 minor release

--- OptimizelySDK.DemoApp/Properties/AssemblyInfo.cs
+++ OptimizelySDK.DemoApp/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0")] // Used by NuGet.
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyInformationalVersion("4.1.0")] // Used by NuGet.
