800a35580188cbb2053a222fc6d3bc2e8265e996
Md Junaed Hossain <169046794+junaed-optimizely@users.noreply.github.com>
2026-09-03 19:06:48 +0600

    [AI-FSSDK] [FSSDK-13070] prepare for release csharp v4.5.0 (#421)

--- OptimizelySDK.Tests/Properties/AssemblyInfo.cs
+++ OptimizelySDK.Tests/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.4.0.0")]
-[assembly: AssemblyFileVersion("4.4.0.0")]
-[assembly: AssemblyInformationalVersion("4.4.0")] // Used by NuGet.
+[assembly: AssemblyVersion("4.5.0.0")]
+[assembly: AssemblyFileVersion("4.5.0.0")]
+[assembly: AssemblyInformationalVersion("4.5.0")] // Used by NuGet.
